9ca340a39466d0b11234b95a5f566720d527f8a1
JermaineMckenna <jermaine.mckenna_@outlook.com>
2025-09-25 12:11:36 +0100

    home,menu,booking poages all linked and intergrated to views.py file and path included to urls file and also menu data included to urls to transfer data

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home_view(request):
+    return render(request, 'menu/home.html')
+
+def menu_view(request):
+    return render(request, 'menu/menu.html')
+
+def booking_view(request):
+    return render(request, 'menu/booking.html')
